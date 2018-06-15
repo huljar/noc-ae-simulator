bbd1ddb78b2d060440bc8373f4ad6c311513ccff
Julian Harttung <julian.harttung@web.de>
2018-06-15 19:17:49 +0200

    Added analysis methodology for number of enc/auth units

--- analysis/main.py
+++ analysis/main.py
@@ -119,6 +119,73 @@ def getNumberOfGeneratedArqs(cursorVec):
         totalArqs += ni[0]
     return totalArqs
 
+def getQueueTimesEncAuthModules(cursorSca):
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].encryptQueue', 'scaName': 'timeInQueue:max'}
+    )
+    encQueueMax = max(cursorSca.fetchall(), key=lambda row: row[0])[0]
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].decryptQueue', 'scaName': 'timeInQueue:max'}
+    )
+    decQueueMax = max(cursorSca.fetchall(), key=lambda row: row[0])[0]
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].authenticateQueue', 'scaName': 'timeInQueue:max'}
+    )
+    authQueueMax = max(cursorSca.fetchall(), key=lambda row: row[0])[0]
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].verifyQueue', 'scaName': 'timeInQueue:max'}
+    )
+    verQueueMax = max(cursorSca.fetchall(), key=lambda row: row[0])[0]
+
+    encMax = max(encQueueMax, decQueueMax)
+    authMax = max(authQueueMax, verQueueMax)
+
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].encryptQueue', 'scaName': 'timeInQueue:avg'}
+    )
+    encQueueAvg = np.mean(cursorSca.fetchall())
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].decryptQueue', 'scaName': 'timeInQueue:avg'}
+    )
+    decQueueAvg = np.mean(cursorSca.fetchall())
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].authenticateQueue', 'scaName': 'timeInQueue:avg'}
+    )
+    authQueueAvg = np.mean(cursorSca.fetchall())
+    cursorSca.execute(
+            '''select scalarValue
+               from scalar
+               where moduleName like :modName and scalarName = :scaName''',
+            {'modName': 'Mesh2D.ni[%].verifyQueue', 'scaName': 'timeInQueue:avg'}
+    )
+    verQueueAvg = np.mean(cursorSca.fetchall())
+
+    encAvg = np.mean([encQueueAvg, decQueueAvg])
+    authAvg = np.mean([authQueueAvg, verQueueAvg])
+
+    return (encMax, authMax, encAvg, authAvg)
+
 def getFlitEndToEndLatency(cursorVec, sourceId, targetId):
     sourceIds = []
     targetIds = []
@@ -172,6 +239,11 @@ if __name__ == '__main__':
     print('Number of flits generated at the processing elements: ' + str(numFlits))
     print('Number of ARQs generated: ' + str(numArqs))
     print('... that means we have ' + str(numArqs / numFlits) + ' ARQs per source flit.\n')
+
+    # Print max/avg queue times for enc/auth queues
+    (encMax, authMax, encAvg, authAvg) = getQueueTimesEncAuthModules(cursorSca)
+    print('Encryption unit wait time: max ' + str(encMax) + ', avg ' + str(encAvg))
+    print('Authentication unit wait time: max ' + str(authMax) + ', avg ' + str(authAvg) + '\n')
 
     # Close DB connections
     dbClose(connSca, connVec)
